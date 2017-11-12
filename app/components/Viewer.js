906b21b509352477305f6a217b03b3b1e818dc6a
Michael Bowman <bowmanmc@gmail.com>
2017-11-11 20:31:41 -0500

    render symbols to svg defs section

--- app/components/Viewer.js
+++ app/components/Viewer.js
@@ -5,12 +5,25 @@ class Viewer extends Component {
 
     constructor(props, context) {
         super(props, context);
+
+        this.getSymbols = this.getSymbols.bind(this);
+    }
+
+    getSymbols() {
+        const symbols = this.props.symbols.join('');
+        return {
+            __html: symbols
+        };
     }
 
     render() {
 
         return (
             <div className="Viewer">
+                <svg>
+                    <defs dangerouslySetInnerHTML={this.getSymbols()}>
+                    </defs>
+                </svg>
             </div>
         );
     }
